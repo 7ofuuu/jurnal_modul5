060da45920961d94fd87493c9edf8e7db858142f
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 07:53:22 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -32,10 +32,38 @@ class Program
         }
     }
 
+    class SimpleDataBase<T> {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase() {
+          storedData = new List<T>();
+          inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data) { 
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData() {
+            for (int i = 0; i < storedData.Count; i++) {
+                Console.WriteLine($"Data {i+1} berisi: " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+            }
+        }
+    }
+
+
     static void Main()
     {
         PemrosesData data = new PemrosesData();
         data.DapatkanNilaiTerbesar<double, double, double>(10, 30, 22);
+
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddNewData(12);
+        db.AddNewData(34);
+        db.AddNewData(56);
+        db.PrintAllData();
     }
 
 }
